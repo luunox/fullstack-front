217051c23ce8f7a2b092af926e084f608628d383
luunox <kevinoxxd@gmail.com>
2023-02-24 03:52:08 -0500

    Var

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -47,7 +47,9 @@ const Dashboard = () => {
 					<p className="text-base font-semibold">Clientes después de los 18 años</p>
 				</Col>
 			</Row>
-      {process.env.KENDO_UI_LICENSE}
+      <Row>
+        env var {process.env.KENDO_UI_LICENSE}
+      </Row>
 		</>
 	);
 };
